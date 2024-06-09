85041ac138899558e1ed299ca816bd0e2a5e1d51
InkyungLee <yklee980216@naver.com>
2024-06-09 23:30:45 +0900

    no message

---
+++ DeployTest1/src/main/java/com/kh/test/controller/MemberController.java
@@ -0,0 +1,28 @@
+package com.kh.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+
+public class MemberController {
+	@PostMapping("loginTest")
+	public String loginTest(
+			@RequestParam("id") String id,
+			@RequestParam("pw") String pw,
+			Model model
+			) {
+		String result = null;
+		if(id.equals("user01") && pw.equals("pass01")) {
+			result = "성공";
+		}else {
+			result = "실패";
+		}
+		model.addAttribute("result", result);
+		// 타임리프 접두사 : classpath:/templates
+		// 타임리프 접미사 : .html
+		return "loginResult";
+	}
+}
